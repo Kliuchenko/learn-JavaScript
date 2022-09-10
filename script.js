fd5c2f03d484ea86dc89772179f46e60c9ab594c
Kliuchenko <pasha.kluchenko@gmail.com>
2022-09-10 19:16:18 +0300

    solve first exercise

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Write a JavaScript function to check whether an `input` is a string or not.
+function isString(arg) {
+    if (typeof arg === "string") {
+        return true;
+    }
+    return false;
+}
