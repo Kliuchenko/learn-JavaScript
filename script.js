a0236e6aef657686b2ad6502700190a28753eec9
Kliuchenko <pasha.kluchenko@gmail.com>
2022-09-11 16:44:06 +0300

    add 2 exercise

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // 1. Write a JavaScript function to check whether an `input` is a string or not.
-function isString(arg) {
-    if (typeof arg === "string") {
-        return true;
+// function isString(arg) {
+//     if (typeof arg === "string") {
+//         return true;
+//     }
+//     return false;
+// }
+// ______________________________________________________________
+// 2. Write a JavaScript function to check whether a string is blank or not
+function isBlanc(string) {
+    if (string === "") {
+        return true
     }
-    return false;
+    return false
 }
+// ______________________________________________________________
+// ______________________________________________________________
+// ______________________________________________________________
+// ______________________________________________________________
+// ______________________________________________________________
+// ______________________________________________________________
+// ______________________________________________________________
